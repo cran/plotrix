296187b5de27fc39d62e7c3bc23dcd7f0b1e31ec
Jim Lemon <jim.lemon@uts.edu.au>
2005-05-16 00:00:00 +0000

    version 1.3-1

--- R/color.scale.R
+++ R/color.scale.R
@@ -1,21 +1,20 @@
-color.scale<-function(x,redrange,greenrange,bluerange,scale.up=FALSE){
+color.scale<-function(x,redrange,greenrange,bluerange){
  ncolors<-length(x)
  if(length(redrange) > 1) {
   reds<-rescale(x,redrange)
-  if(min(reds) < 0 || max(reds) > 255) reds<-rescale(reds,c(0,255))
+  if(min(reds) < 0 || max(reds) > 1) reds<-rescale(reds,c(0,1))
  }
  else reds<-rep(redrange,ncolors)
  if(length(greenrange) > 1) {
   greens<-rescale(x,greenrange)
-  if(min(greens) < 0 || max(greens) > 255) greens<-rescale(greens,c(0,255))
+  if(min(greens) < 0 || max(greens) > 1) greens<-rescale(greens,c(0,1))
  }
  else greens<-rep(greenrange,ncolors)
  if(length(bluerange) > 1) {
   blues<-rescale(x,bluerange)
-  if(min(blues) < 0 || max(blues) > 255) blues<-rescale(blues,c(0,255))
+  if(min(blues) < 0 || max(blues) > 1) blues<-rescale(blues,c(0,1))
  }
  else blues<-rep(bluerange,ncolors)
- colormatrix<-cbind(reds,greens,blues)
- colvec<-apply(colormatrix,1,rgb.to.hex,scale.up)
+ colvec<-rgb(reds,greens,blues)
  return(colvec)
 }
---
+++ R/textbox.R
@@ -0,0 +1,22 @@
+textbox<-function(x,y,textlist,justify=TRUE,cex=1,leading=0.5,box=TRUE) {
+ par(adj = 0)
+ textstr <- paste(textlist, collapse = " ")
+ words <- strsplit(textstr, " ")[[1]]
+ line.height <- strheight("hy", cex = cex) * (1 + leading)
+ x.len <- diff(x)
+ y.pos <- y
+ x.pos <- x[1]
+ curword <- 1
+ while (curword < length(words)) {
+  curline <- ""
+  curline <- paste(curline, words[curword])
+  curword <- curword + 1
+  while (x.pos+strwidth(paste(curline,words[curword]),cex=cex) < x[2]) {
+   curline<-paste(curline,words[curword])
+   curword<-curword+1
+  }
+  text(x.pos,y.pos,curline)
+  y.pos<-y.pos-line.height
+ }
+ if(box) rect(x[1],y+line.height,x[2],y.pos)
+}
