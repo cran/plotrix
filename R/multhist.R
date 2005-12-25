e4e1ccf5602b453e18bce39fc9cf7bc2992a6ff7
Jim Lemon <bitwrit@ozemail.com.au>
2005-12-25 00:00:00 +0000

    version 2.0

--- R/multhist.R
+++ R/multhist.R
@@ -1,5 +1,5 @@
-multhist<-function(x,...) {
- allhist<-hist(unlist(x),plot=FALSE)
+multhist<-function(x,breaks="Sturges",...) {
+ allhist<-hist(unlist(x),breaks=breaks,plot=FALSE)
  combhist<-
   t(sapply(x,function(z) hist(z,breaks=allhist$breaks,plot=FALSE)$counts))
  barplot(combhist,beside=TRUE,names=signif(allhist$mids,2),...)
