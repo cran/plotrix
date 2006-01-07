b287eb7c4939ce6556487ca336ce83b71face0d3
Jim Lemon <bitwrit@ozemail.com.au>
2006-01-07 02:44:33 +0000

    version 2.0.1

---
+++ R/color2D.matplot.R
@@ -0,0 +1,33 @@
+color2D.matplot<-function(x,redrange=c(0,1),greenrange=c(0,1),bluerange=c(0,1),
+ show.legend=FALSE,...) {
+ 
+ if(is.matrix(x) || is.data.frame(x)) {
+  xdim<-dim(x)
+  if(is.data.frame(x)) x<-unlist(x)
+  else x<-as.vector(x)
+  oldpar<-par(no.readonly=TRUE)
+  par(xaxs="i",yaxs="i")
+  plot(c(0,xdim[2]),c(0,xdim[1]),type="n",axes=FALSE,...)
+  box()
+  axis(1,at=pretty(0:xdim[2])[-1]-0.5,labels=pretty(0:xdim[2])[-1])
+  yticks<-pretty(0:xdim[1])[-1]
+  axis(2,at=xdim[1]-yticks+0.5,yticks)
+  cellcolors<-color.scale(as.vector(x),redrange,greenrange,bluerange)
+  # start from the top left - isomorphic with the matrix layout
+  rect(sort(rep((1:xdim[2])-1,xdim[1])),rep(seq(xdim[1]-1,0,by=-1),xdim[2]),
+   sort(rep(1:xdim[2],xdim[1])),rep(seq(xdim[1],1,by=-1),xdim[2]),
+   col=cellcolors,border=FALSE)
+  grx1<--xdim[1]/15
+  gry1<--xdim[2]/5.5
+  grx2<-grx1+xdim[1]/3
+  gry2<-gry1+xdim[2]/20
+  if(show.legend) {
+   par(xpd=TRUE)
+   gradient.rect(grx1,gry1,grx2,gry2,redrange,greenrange,bluerange,nslices=10)
+   par(xpd=FALSE)
+   mtext(round(range(x),2),1,2,at=c(grx1,grx2))
+  }
+  par(oldpar)
+ }
+ else cat("x must be a data frame or matrix\n")
+}
--- R/spread.labels.R
+++ R/spread.labels.R
@@ -1,15 +1,16 @@
-spread.labels<-function(x,y,labels=NULL,offset,col="white",border=FALSE,...) {
+spread.labels<-function(x,y,labels=NULL,offset,bg="white",border=FALSE,...) {
  if(missing(x))
-  stop("Usage: spread.labels(x,y,labels=NULL,offset,col=\"white\",...)")
+  stop("Usage: spread.labels(x,y,labels=NULL,offset,bg=\"white\",...)")
  if(diff(range(x)) < diff(range(y))) {
   sort.index<-sort.list(y)
   x<-x[sort.index]
   y<-y[sort.index]
   ny<-length(y)
+  if(missing(offset)) offset<-diff(par("usr")[1:2])/4
   offsets<-rep(c(offset,-offset),ny/2+1)[1:ny]
   newy<-seq(y[1],y[ny],length=ny)
   segments(x+offsets,newy,x,y)
-  boxed.labels(x+offsets,newy,labels,col=col,border=border,...)
+  boxed.labels(x+offsets,newy,labels,bg=bg,border=border,...)
  }
  else {
   sort.index<-sort.list(x)
@@ -19,6 +20,6 @@ spread.labels<-function(x,y,labels=NULL,offset,col="white",border=FALSE,...) {
   offsets<-rep(c(offset,-offset),nx/2+1)[1:nx]
   newx<-seq(x[1],x[nx],length=nx)
   segments(newx,y+offsets,x,y)
-  boxed.labels(newx,y+offsets,labels,col=col,border=border,...)
+  boxed.labels(newx,y+offsets,labels,bg=bg,border=border,...)
  }
 }
