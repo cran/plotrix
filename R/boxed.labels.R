375d31ee59707f8923ed70ddb80215282206e1b4
Jim Lemon <jim@bitwrit.com.au>
2007-01-16 00:00:00 +0000

    version 2.3

--- R/boxed.labels.R
+++ R/boxed.labels.R
@@ -1,14 +1,20 @@
 # x,y values are the center of the labels
-boxed.labels<-function(x,y=NA,labels,bg="white",border=TRUE,
- xpad=0.6,ypad=0.6,...) {
+boxed.labels<-function(x,y=NA,labels,bg="white",border=TRUE,xpad=0.6,
+ ypad=0.6,srt=0,...) {
 
  if(is.na(y) && is.list(x)) {
   y<-unlist(x[[2]])
   x<-unlist(x[[1]])
  }
- widths<-strwidth(labels)
- heights<-strheight(labels)
+ if(srt==90 || srt==270) {
+  heights<-strwidth(labels)
+  widths<-strheight(labels)
+ }
+ else{
+  widths<-strwidth(labels)
+  heights<-strheight(labels)
+ }
  rect(x-widths*xpad,y-heights*ypad,x+widths*xpad,y+heights*ypad,
   col=bg,border=border)
- text(x,y,labels,...)
+ text(x,y,labels,srt=srt,...)
 }
