18b20995a94d14c7792827e961898b9b225d7451
Jim Lemon <jim@bitwrit.com.au>
2009-01-22 00:00:00 +0000

    version 2.5-1

--- R/intersectDiagram.R
+++ R/intersectDiagram.R
@@ -1,33 +1,34 @@
-intersectDiagram<-function(x,pct=FALSE,show.nulls=FALSE,mar=c(0,0,3,0),
- main="Intersection Diagram",col=NULL,minspacing=0.1) {
+intersectDiagram<-function(x,pct=FALSE,show.nulls=FALSE,xnames=NULL,
+ namesep="-",mar=c(0,0,3,0),main="Intersection Diagram",col=NULL,
+ minspacing=0.1) {
 
  if(is.matrix(x) || is.data.frame(x)) x<-makeIntersectList(x)
+ # check if this is going to screw up some other list that would work
+ if(!match(class(x),"intersectList",0))
+  stop("x must be a matrix, data frame or intersectList")
  oldmar<-par("mar")
  par(mar=mar)
- # don't count the total in the last element of the list
+ # don't count the total of the last element of the list
  lenx<-length(x)-1
  xtotal<-x[[length(x)]]
- xnames<-names(x[[1]])
+ if(is.null(xnames)) xnames<-names(x[[1]])
  if(is.null(xnames)) xnames<-LETTERS[1:lenx]
  if(is.null(col)) col<-c(rainbow(length(x)-1),NA)
- listsums<-sapply(x,sum)
+ if(length(col) < length(x)) col<-rep(col,length.out=length(x))
+ listsums<-sapply(x, sum)
  horizmax<-max(listsums)
  xsum<-0
  plot(0,xlim=c(0,horizmax*(1+minspacing)),ylim=c(0,lenx+show.nulls),
   main=main,xlab="",ylab="",type="n",axes=FALSE)
  for(comb in 1:lenx) {
   xsum<-xsum+sum(x[[comb]])
-  blocknames<-names(x[[comb]])
-  if(is.null(blocknames)) {
-   rowmat<-combn(xnames,comb)
-   blocknames<-pasteCols(rowmat)
-  }
+  rowmat<-combn(xnames,comb)
+  blocknames<-pasteCols(rowmat,namesep)
   lenxcomb<-length(x[[comb]])
   gap<-(horizmax*(1+minspacing)-sum(x[[comb]]))/lenxcomb
   startx<-gap/2
   for(intersect in 1:lenxcomb) {
-   cellqnt<-ifelse(pct,
-    paste(round(100*x[[comb]][intersect]/xtotal,1),"%",sep=""),
+   cellqnt<-ifelse(pct, paste(round(100*x[[comb]][intersect]/xtotal,1),"%",sep=""),
     x[[comb]][intersect])
    if(x[[comb]][intersect] > 0) {
     if(!is.na(col[1])) {
@@ -37,31 +38,29 @@ intersectDiagram<-function(x,pct=FALSE,show.nulls=FALSE,mar=c(0,0,3,0),
      leftx<-startx
      for(bn in 1:lencol) {
       if(length(grep(xnames[bn],blocknames[intersect],fixed=TRUE))) {
-       polygon(c(leftx,leftx,leftx+xinc,leftx+xinc),
-        c(lenx+show.nulls-comb+0.1,lenx+show.nulls-comb+0.9,
-	lenx+show.nulls-comb+0.9,lenx+show.nulls-comb+0.1),
-	border=NA,col=col[bn])
-        slice<-slice+1
-        leftx<-leftx+xinc
+       polygon(c(leftx,leftx,leftx+xinc,leftx+xinc),c(lenx+show.nulls-comb+0.1,
+        lenx+show.nulls-comb+0.9,lenx+show.nulls-comb+0.9,
+        lenx+show.nulls-comb+0.1),border=NA,col=col[bn])
+       slice<-slice+1
+       leftx <- leftx + xinc
       }
      }
     }
-    rect(startx,lenx+show.nulls-comb+0.1,
-     startx+x[[comb]][intersect],lenx+show.nulls-comb+0.9)
+    rect(startx,lenx+show.nulls-comb+0.1,startx+x[[comb]][intersect],
+     lenx+show.nulls-comb+0.9)
    }
    boxed.labels(startx+x[[comb]][intersect]/2,lenx+show.nulls-comb+0.5,
-    paste(blocknames[intersect],cellqnt,sep="\n"))
+   paste(blocknames[intersect],cellqnt,sep="\n"))
    startx<-startx+x[[comb]][intersect]+gap
   }
  }
  if(show.nulls) {
-  leftx<-sum(par("usr")[1:2])/2-x[[lenx]]/2
-   polygon(c(leftx,leftx,leftx+x[[lenx]],leftx+x[[lenx]]), 
-    c(0.1,0.9,0.9,0.1),col=NA)
-   cellqnt<-xtotal-xsum
-   if(pct) cellqnt<-paste(round(100*cellqnt/xtotal,1),"%",sep="")
-   boxed.labels(leftx+x[[lenx]]/2,0.5,
-    paste("Non-members",cellqnt,sep="\n"),col=col[length(col)])
+  nonset<-as.numeric(xtotal-xsum)
+  leftx<-sum(par("usr")[1:2])/2-nonset/2
+  polygon(c(leftx,leftx,leftx+nonset,leftx+nonset),c(0.1,0.9,0.9,0.1),col=NA)
+  if(pct) nonsetpc<-paste(round(100*nonset/xtotal,1),"%",sep="")
+  boxed.labels(leftx+nonset/2,0.5,paste("Non-members",nonsetpc,sep="\n"),
+   col=col[length(col)])
  }
  par(mar=oldmar)
 }
--- R/makeIntersectList.R
+++ R/makeIntersectList.R
@@ -1,27 +1,30 @@
-makeIntersectList<-function(x) {
- xnames<-names(x)
+makeIntersectList<-function(x,xnames=NULL) {
+ # if no names passed, see if x is a data frame with names                                                                                           
+ if(is.null(xnames)) xnames<-names(x)                     
+ # coerce x to a matrix                                                 
  if(!is.matrix(x)) x<-as.matrix(x)
- else xnames<-colnames(x)
+ # if it was already a matrix and xnames is still NULL, try colnames                                                                   
+ if(is.null(xnames)) xnames<-colnames(x)                                                              
  dimx<-dim(x)
- if(is.null(xnames)) xnames<-LETTERS[1:dimx[2]]
- intersectList<-vector("list",dimx[2] + 1)
+ # if no names available, use capital letters                                                                          
+ if(is.null(xnames)) xnames<-LETTERS[1:dimx[2]]                                                        
+ intersectList<-vector("list",dimx[2]+1)                                            
  for(intersect in 1:dimx[2])
-  intersectList[[1]][intersect]<-sum(rowSums(x) == 1 & x[,intersect])
- names(intersectList[[1]])<-xnames
- for(comb in 2:dimx[2]) {
-  nn<-choose(dimx[2],comb)
-  intersectList[[comb]]<-rep(0,nn)
+  intersectList[[1]][intersect]<-sum(rowSums(x) == 1 & x[,intersect])                                                                 
+ names(intersectList[[1]])<-xnames                                                     
+ for(comb in 2:dimx[2]) {                                                               
+  nn<-choose(dimx[2],comb)                                                         
+  intersectList[[comb]]<-rep(0,nn)                                                 
   currentnames<-names(intersectList[[comb]])<-pasteCols(combn(xnames,comb))
   currentcombs<-combn(1:dimx[2],comb,simplify=TRUE)
   for(intersect in 1:nn) {
-   combvec<-rep(0, dimx[2])
+   combvec<-rep(0,dimx[2])
    combvec[currentcombs[,intersect]]<-1
-   intersectList[[comb]][intersect]<-
-    sum(colSums(apply(x,1,"==",combvec))==dimx[2])
+   intersectList[[comb]][intersect]<-sum(colSums(apply(x,1,"==",combvec))==dimx[2])
   }
  }
- intersectList[[dimx[2] + 1]] <- dimx[1]
- names(intersectList[[dimx[2] + 1]]) <- "Total"
+ intersectList[[dimx[2]+1]]<-dimx[1]
+ names(intersectList[[dimx[2]+1]])<-"Total"
+ class(intersectList)<-"intersectList"
  return(intersectList)
 }
- 
