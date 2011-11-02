0459d9bb068004803b6b7b20e7464bca2e24ba55
Jim Lemon <jim@bitwrit.com.au>
2011-11-02 00:00:00 +0000

    version 3.2-8

--- R/getIntersectList.R
+++ R/getIntersectList.R
@@ -1,28 +1,31 @@
-# getIntersectList
-# Allows the user to manually enter the number of data objects with each
-# combination of intersections of the nelem sets
-
-getIntersectList<-function(nelem,xnames=NULL,sep="-") {
- if(is.null(xnames)) xnames<-LETTERS[1:nelem]
- xnamelen<-length(xnames)
- if(xnamelen < nelem) {
+getIntersectList<-function(nelem,xnames=NULL,sep="+") {
+if(is.null(xnames)) xnames<-LETTERS[1:nelem]
+xnamelen<-length(xnames)
+if(xnamelen < nelem) {
   extranames<-paste("extra",1:(nelem-xnamelen),sep=sep)
   cat("Not enough names in",xnames,"adding",extranames,"\n")
   xnames<-c(xnames,extranames)
- }
- intersectList<-vector("list",nelem+1)
- for(comb in 1:nelem) {
+}
+intersectList<-vector("list",nelem+2)
+total_n<-0
+for(comb in 1:nelem) {
   nn<-choose(nelem,comb)
   intersectList[[comb]]<-rep(0,nn)
   currentnames<-names(intersectList[[comb]])<-
    pasteCols(combn(xnames,comb),sep=sep)
   for(intersect in 1:nn) {
    cat("Number of elements in",currentnames[intersect],"- ")
-   intersectList[[comb]][intersect]<-scan(nmax=1,quiet=TRUE)
+   current_n<-scan(nmax=1,quiet=TRUE)
+   intersectList[[comb]][intersect]<-current_n
+   total_n<-total_n+current_n
   }
- }
- cat("Total number of elements - ")
- intersectList[[nelem+1]]<-scan(nmax=1,quiet=TRUE)
- class(intersectList)<-"intersectList"
- return(intersectList)
+}
+cat("Total number of elements (press Enter for ",total_n,") - ",
+  sep="")
+intersectList[[nelem + 1]]<-scan(nmax=1,quiet=TRUE)
+if(length(intersectList[[nelem+1]])==0)
+  intersectList[[nelem+1]]<-total_n
+intersectList[[nelem+2]]<-xnames
+class(intersectList) <- "intersectList"
+return(intersectList)
 }
