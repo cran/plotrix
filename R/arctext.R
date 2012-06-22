61229d48a57845038447d7c9e26cd65f10686531
Jim Lemon <jim@bitwrit.com.au>
2012-06-22 00:00:00 +0000

    version 3.4-2

--- R/arctext.R
+++ R/arctext.R
@@ -1,23 +1,28 @@
-arctext<-function(x,center=c(0,0),radius=1,start=NA,middle=pi/2,
- stretch=1,cex=1,...) {
- 
- oldcex<-par("cex")
+arctext<-function(x,center=c(0,0),radius=1,start=NA,middle=pi/2,end=NA,stretch=1,
+ cex=NA, ...) {
+
+ oldcex <- par("cex")
+ if (is.na(cex)) cex <- oldcex
  par(cex=cex)
- xvec<-strsplit(x,"")[[1]]
- lenx<-length(xvec)
- xwidths<-stretch*strwidth(xvec)
- charangles<-xwidths/radius
- changrang<-range(charangles)
- if(changrang[2]/changrang[1] > 2)
-  charangles[charangles < changrang[2]/2]<-changrang[2]/2
- if(is.na(start)) start<-middle+sum(xwidths)/(radius*2)
- charpos<-c(start,start-cumsum(charangles)[-lenx])
+ xvec <- strsplit(x, "")[[1]]
+ lenx <- length(xvec)
+ xwidths <- stretch * strwidth(xvec)
+ charangles <- xwidths/radius
+ changrang <- range(charangles)
+ charangles[charangles < changrang[2]/2] <- changrang[2]/2 # Make really narrow characters wider
+ if (!is.na(end)) start <- end + sum(charangles)
+ if (is.na(start)) start <- middle + sum(charangles)/2
+ charstart <- c(start, start - cumsum(charangles)[-lenx])
+ charpos <- charstart - charangles/2
  xylim <- par("usr")
  plotdim <- par("pin")
  ymult <- (xylim[4] - xylim[3])/(xylim[2] - xylim[1]) * plotdim[1]/plotdim[2]
- for(xchar in 1:lenx)
-  text(center[1]+radius*cos(charpos[xchar]),
-   center[2]+radius*sin(charpos[xchar])*ymult,xvec[xchar],
-   adj=c(0.5,0.5),srt=180*(charpos[xchar]-middle)/pi,...)
+ for (xchar in 1:lenx) {
+  srt <- 180*charpos[xchar]/pi - 90
+  text(center[1] + radius * cos(charpos[xchar]), 
+   center[2] + radius * sin(charpos[xchar]) * ymult, xvec[xchar], 
+   adj=c(0.5, 0.5), srt=srt, ...)
+ }
  par(cex=oldcex)
 }
+
--- R/spread.labels.R
+++ R/spread.labels.R
@@ -3,26 +3,41 @@ spread.labels<-function (x,y,labels=NULL,ony=NA,offsets=NA,between=FALSE,
 
  if(missing(x)) 
   stop("Usage: spread.labels(x,y,labels,...)")
+ nx<-length(x)
  ny<-length(y)
+ if(is.na(ony)) {
+  xylim<-par("usr")
+  ony<-diff(range(x))/(xylim[2]-xylim[1])<diff(range(y))/(xylim[4]-xylim[3])
+ }
  if(between) {
   if(length(linecol)==1) linecol<-rep(linecol,2)
   nlabels<-length(labels)
-  newy<-seq(y[1],y[ny],length=nlabels)
-  # put the labels in the middle
-  labelx<-rep(mean(x),nlabels)
-  # do the left lines
-  segments(x[1:nlabels],y[1:nlabels],labelx-strwidth(labels)/2,newy,
-   col=linecol[1])
-  # now the right lines
-  segments(x[(nlabels+1):ny],y[(nlabels+1):ny],labelx+strwidth(labels)/2,newy,
-   col=linecol[2])
-  text(labelx,newy,labels,srt=srt,...)
+  if(ony) {
+   newy<-seq(y[1],y[ny],length=nlabels)
+   # put the labels in the middle
+   labelx<-rep(mean(x),nlabels)
+   # do the left lines
+   segments(x[1:nlabels],y[1:nlabels],labelx-strwidth(labels)/2,newy,
+    col=linecol[1])
+   # now the right lines
+   segments(x[(nlabels+1):ny],y[(nlabels+1):ny],labelx+strwidth(labels)/2,newy,
+    col=linecol[2])
+   text(labelx,newy,labels,srt=srt,...)
+  }
+  else {
+   newx<-seq(x[1],x[nx],length=nlabels)
+   # put the labels in the middle
+   labely<-rep(mean(y),nlabels)
+   # do the upper lines
+   segments(x[1:nlabels],y[1:nlabels],newx+strheight(labels)/2,labely,
+    col=linecol[1])
+   # now the right lines
+   #segments(x[(nlabels+1):ny],y[(nlabels+1):ny],labelx+strwidth(labels)/2,newy,
+    #col=linecol[2])
+   text(newx,labely,labels,srt=srt,...)
+  }
  }
  else {
-  if(is.na(ony)) {
-   xylim<-par("usr")
-   ony<-diff(range(x))/(xylim[2]-xylim[1])<diff(range(y))/(xylim[4]-xylim[3])
-  }
   if(ony) {
    sort.index<-sort.list(y)
    x<-x[sort.index]
@@ -46,8 +61,9 @@ spread.labels<-function (x,y,labels=NULL,ony=NA,offsets=NA,between=FALSE,
     offsets<-rep(c(offset,-offset),nx/2+1)[1:nx]
    }
    text(newx,y+offsets,labels[sort.index],srt=srt,...)
-   seggap<-strheight("o")
-   if(all(offsets > 0) || all(offsets < 0)) seggaps<-rep(seggap,length.out=nx)
+   seggap<-strheight("M")*2
+   if(all(offsets > 0) || all(offsets < 0))
+    seggaps<-rep(seggap*(all(offsets)<0)*-1,length.out=nx)
    else seggaps<-rep(c(seggap,-seggap),length.out=nx)
    segments(newx,y+offsets-seggaps,x,y)
   }
